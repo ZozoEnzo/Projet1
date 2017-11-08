84b35b373088452f05662d71d0d25d6e3bcd61bf
Seratto <32639959+Seratto@users.noreply.github.com>
2017-11-08 11:52:08 +0100

    structure MVC + debut menu

---
+++ index.php
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <head>
+        <title>GSB project</title>
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+    </head>
+    <body>
+        <div name="haut">
+            <h1><img src="logo.jpg" width="100" height="60"/>Gestion des visites</h1>
+        </div>
+        <div name="gauche">
+            <h2>Outils</h2>
+            <ul>
+                <li>Comptes-Rendus</li>
+                <ul>
+                    <li><a href="formRAPPORT_VISITE.htm" >Nouveaux</a></li>
+                    <li>Consulter</li>
+                </ul>
+                <li>Consulter</li>
+                <ul>
+                    <li><a href="formMEDICAMENT.htm" >Médicaments</a></li>
+                    <li><a href="formPRATICIEN.htm" >Praticiens</a></li>
+                    <li><a href="formVISITEUR.htm" >Autres visiteurs</a></li>
+                </ul>
+            </ul>
+        </div>
+        <div name="droite">
+            <div name="bas"></div>
+        </div>
+    </body>
+</html>
---
+++ vue/menu.inc.php
@@ -0,0 +1,13 @@
+<ul>
+    <li>Comptes-Rendus</li>
+    <ul>
+        <li><a href="formRAPPORT_VISITE.htm" >Nouveaux</a></li>
+        <li>Consulter</li>
+    </ul>
+    <li>Consulter</li>
+    <ul>
+        <li><a href="formMEDICAMENT.htm" >Médicaments</a></li>
+        <li><a href="formPRATICIEN.htm" >Praticiens</a></li>
+        <li><a href="formVISITEUR.htm" >Autres visiteurs</a></li>
+    </ul>
+</ul>
