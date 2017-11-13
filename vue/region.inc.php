dfae73ce7a8af02c12fc1b708e514f4f86edcea0
zozo78team <enzo.borges@hotmail.fr>
2017-11-13 16:23:15 +0100

    Indentation supprimé

--- vue/region.inc.php
+++ vue/region.inc.php
@@ -1,22 +1,22 @@
-				<h2>Listes des régions</h2>
-				<a class="btn" href='index.php?uc=Region&action=unique'>Séléctionner cette région</a>
-				<section>
-					<table><tr><th>Code Région</th><th>Code Section</th><th>Nom Région</th><th>Action</th></tr>
-						<?php
-							foreach($lesRegions as $Region)
-							{
-								$id = $Region->getCode();
-								$nom = $Region->getNom();
-								$sec = $Region->getCodeSec();
-								?>
-								<tr>
-									<td width=5%><?php echo $id; ?></td><td><?php echo $nom; ?></td><td><?php echo $sec; ?></td>
-									<td class='action' width=15%>
-										<a href='index.php?uc=Region&action=select&numreg=<?php echo $id; ?>' class="imageSelectionner" title="Selectionner la région">	</a>
-									</td>
-								</tr>
-							<?php
-							}
-						?>
-					</table>
-				</section>
+<h2>Listes des régions</h2>
+<a class="btn" href='index.php?uc=Region&action=unique'>Séléctionner cette région</a>
+<section>
+	<table><tr><th>Code Région</th><th>Code Section</th><th>Nom Région</th><th>Action</th></tr>
+		<?php
+			foreach($lesRegions as $Region)
+			{
+				$id = $Region->getCode();
+				$nom = $Region->getNom();
+				$sec = $Region->getCodeSec();
+				?>
+				<tr>
+					<td width=5%><?php echo $id; ?></td><td><?php echo $nom; ?></td><td><?php echo $sec; ?></td>
+					<td class='action' width=15%>
+						<a href='index.php?uc=Region&action=select&numreg=<?php echo $id; ?>' class="imageSelectionner" title="Selectionner la région">	</a>
+					</td>
+				</tr>
+			<?php
+			}
+		?>
+	</table>
+</section>
