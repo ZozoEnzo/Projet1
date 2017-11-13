0f16b00b9782e950cd332af839c5150fa41ee092
zozo78team <enzo.borges@hotmail.fr>
2017-11-13 16:38:53 +0100

    Meilleur tableau

--- vue/region.inc.php
+++ vue/region.inc.php
@@ -1,16 +1,14 @@
 <h2>Listes des régions</h2>
-<a class="btn" href='index.php?uc=Region&action=unique'>Séléctionner cette région</a>
 <section>
-	<table><tr><th>Code Région</th><th>Code Section</th><th>Nom Région</th><th>Action</th></tr>
+	<table><tr><th>Région</th><th>Nombre de visiteurs</th><th>Nombre de délégués</th><th>Action</th></tr>
 		<?php
 			foreach($lesRegions as $Region)
 			{
 				$id = $Region->getCode();
 				$nom = $Region->getNom();
-				$sec = $Region->getCodeSec();
 				?>
 				<tr>
-					<td width=5%><?php echo $id; ?></td><td><?php echo $nom; ?></td><td><?php echo $sec; ?></td>
+					<td><?php echo $nom; ?></td>
 					<td class='action' width=15%>
 						<a href='index.php?uc=Region&action=select&numreg=<?php echo $id; ?>' class="imageSelectionner" title="Selectionner la région">	</a>
 					</td>
