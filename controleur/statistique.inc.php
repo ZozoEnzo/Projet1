68993bee725ab2e7cebacc968b3a9a5e49d94d71
Seratto <32639959+Seratto@users.noreply.github.com>
2017-11-14 11:42:53 +0100

    correction structure controleur

--- controleur/statistique.inc.php
+++ controleur/statistique.inc.php
@@ -1,9 +1,23 @@
 <?php
     if(!isset($_GET["action"])) //Si aucune action n'a été demander, alors on affiche la page d'acceuil des statistiques
+    {
         include("vue/statistique.inc.php");
+    }
     else
+    {
         switch($_GET["action"]) //suivant l'action à réaliser,
         {
-
+            case 'region':
+				$lesRegions = Region::allRegion();
+                include("vue/v_regions.php");
+				break;
+            case 'delegues':
+				$lesDeleg=Visiteur::getDelegues();
+                            //inclure la vue visiteur
+				break;
+            case 'secteur':
+				include("controleur/c_Secteur.php");
+				break;
         }
+    }
 ?>
