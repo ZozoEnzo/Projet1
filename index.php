03180f59392af14a68e5616372783a3b46851ee1
Methionylalanylthreonylserylarginylglyc <ryanlab03@gmail.com>
2017-11-14 11:58:15 +0100

    inclusion des classes dans l'index

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+    require_once("model/class.BD");
+    require_once("model/class.Date.inc.php");
+    require_once("model/class.Departement.inc.php");
+    require_once("model/class.Region.inc.php");
+    require_once("model/class.Secteur.inc.php");
+    require_once("model/class.Visiteur.inc.php");
+?>
 <html id="page">
     <head>
         <meta charset="utf-8" />
