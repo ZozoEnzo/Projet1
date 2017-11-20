84be4605125cdbcae8d88d68c9ab46522228869c
Seratto <32639959+Seratto@users.noreply.github.com>
2017-11-20 14:42:13 +0100

    regionvisiteur

--- model/class.Visiteur.inc.php
+++ model/class.Visiteur.inc.php
@@ -69,13 +69,35 @@
         }
         public static function getSecteur($matricule)
         {
-            $sql="select libelleSecteur from Secteur s, Visiteur v where s.codeSecteur = v.codeSecteur and v.matriculeVisiteur=:matricule";
+            $sql="select libelleSecteur from Secteur s, Visiteur v where s.codeSecteur = v.codeSecteur and v.matriculeVisiteur=':matricule'";
             $resultat=BD::getInstance()->prepare($sql);
             $resultat->bindParam(':matricule', $matricule);
             $resultat->execute();
-            $laLigne=$resultat->fetch(PDO::FETCH_ASSOC);
-            $retour=$laLigne["libelleSecteur"];
-            return $retour;
+            return $resultat;
         }
+
+        public static function getAll()
+        {
+            BD::getInstance()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $req = BD::getInstance()->prepare("SELECT * FROM Secteur");
+            $req->execute();
+            return $req->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,"Visiteur");
+        }
+
+        public static function getAllVisiteurs($region)
+        {
+            $sql="  SELECT  V.matriculeVisiteur, V.codeSecteur, V.codeDep, V.nomVisiteur,
+                            V.adresseVisiteur V.cpVisiteur, V.villeVisiteur, V.dateEmbauche
+                    FROM    Region R, Visiteur V, Travailler T
+                    WHERE   R.codeRegion = T.codeRegion AND
+                            T.matriculeVisiteur = V.matriculeVisiteur AND
+                            R.codeRegion IN (SELECT codeRegion FROM Region WHERE nomRegion=':region')";
+            BD::getInstance()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $req = BD::getInstance()->prepare($sql);
+            $resultat->bindParam(':region', $region);
+            $req->execute();
+            return $req->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,"Visiteur");
+        }
+
     }
 ?>
