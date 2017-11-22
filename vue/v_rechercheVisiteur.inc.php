7e2ac924cd7beed8c86e82b7fb16cd996d8e5f26
Methionylalanylthreonylserylarginylglyc <ryanlab03@gmail.com>
2017-11-22 10:53:14 +0100

    you just loose the game

--- vue/v_rechercheVisiteur.inc.php
+++ vue/v_rechercheVisiteur.inc.php
@@ -1,8 +1,9 @@
-<meta charset="utf-8" />
+<form action='' method='post'>
 <label for="dateDebut">
-    Rechercher les visiteurs ayant commencé à travailler entre le <input type="date" name="dateDebut">
-</label>
+    Rechercher les visiteurs ayant commencé à travailler entre le</label> <input type="date" name="dateDebut">
 <label for="dateFin">
     et le
 </label>
 <input type="date" name="dateFin">
+<input type="submit" value="rechercher">
+</form>
